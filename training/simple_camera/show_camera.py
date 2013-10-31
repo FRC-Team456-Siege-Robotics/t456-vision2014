6d9682f32c8a39542332e19ad102a28f1b7610d7
Jerry Ballard <jerry.ballard@us.army.mil>
2013-10-31 17:35:35 -0500

    Added comments for clarity

--- training/simple_camera/show_camera.py
+++ training/simple_camera/show_camera.py
@@ -3,17 +3,24 @@
 =======================
 =======================
 '''
-import numpy as np
-import cv2
+
+# Import/use system module
 import sys
 
+# Import/use OpenCV module
+import cv2
+
+# Print OpenCV version
 print "OpenCV version:", cv2.__version__
 
-# Capture numbers
+# Capture numbers for video camera
 #  0 = /dev/video0
 #  1 = /dev/video1
 #  etc...
 
+#
+#  Parse the input command line
+#
 if len(sys.argv) == 2:
     vidnum = sys.argv[1]
 else:
@@ -22,16 +29,34 @@ else:
 print "Using /dev/video%d" % int(vidnum)
 print " press 'q' to quit"
 
+#
+#  Setup video capture
+#
 cap = cv2.VideoCapture(int(vidnum))
 
+#
+#  Loop until 'q' is pressed
+#
 while(True):
-    # capture frame by frame
+
+    # capture frame from camera
     ret, frame = cap.read()
 
+    # display on screen camera frame
     cv2.imshow('frame',frame)
+
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
 
 
+#  
+#  Release video capture object
+#
 cap.release()
+
+#
+#  Close and destroy display window
+#
 cv2.destroyAllWindows()
+
+#
