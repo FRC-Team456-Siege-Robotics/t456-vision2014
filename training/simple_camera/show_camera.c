1977dfd9c05b8b297450ae8c8e642717b4cb1859
Jerry Ballard <jerry.ballard@us.army.mil>
2013-11-11 11:39:04 -0600

    error checking for show_camera.c

--- training/simple_camera/show_camera.c
+++ training/simple_camera/show_camera.c
@@ -9,6 +9,8 @@ int main( int argc, char **argv)
    CvCapture*    camera = 0;
    IplImage      *image = 0;
 
+   int camera_img_width, camera_img_height;
+
    /*
    **  Setup camera capture
    **   0 = /dev/video0
@@ -23,6 +25,12 @@ int main( int argc, char **argv)
         printf("camera or image is null\n");
         return;
     }
+    //  Get camera information (image height and width)
+    camera_img_width = cvGetCaptureProperty(camera, CV_CAP_PROP_FRAME_WIDTH);
+    camera_img_height = cvGetCaptureProperty(camera, CV_CAP_PROP_FRAME_HEIGHT);
+
+    printf("camera image width: %d\n", camera_img_width);
+    printf("camera image height: %d\n", camera_img_height);
 
     waitkey_delay = 1.0;
 
